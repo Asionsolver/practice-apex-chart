350fef1d7a1287b45a6812c79f163ebca8bb1da5
asionsolver <asionsolver274@gmail.com>
2024-04-12 17:46:30 +0600

    fully setup new practice project name like apexcharts

---
+++ apex-charts/src/App.jsx
@@ -0,0 +1,13 @@
+import BarChartPartOne from "./components/BarChartPartOne";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center text-blue-800">ApexCharts</h1>
+      <div className="flex justify-center space-x-4">
+        <BarChartPartOne />
+       
+      </div>
+    </>
+  )
+}
